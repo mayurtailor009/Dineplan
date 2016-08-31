1eb40fad63b19fc48034cfbcaca2790b4a99db56
mayur.tailor <mayur.tailor@thepsi.com>
2016-08-31 11:03:13 +0530

    add home activity with side menu drawer.

---
+++ app/src/main/java/com/dineplan/activities/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.dineplan.activities;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.dineplan.R;
+import com.dineplan.utility.Utils;
+
+public class HomeActivity extends BaseActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private Toolbar toolbar;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+        setupDrawer();
+    }
+
+    private void setupDrawer(){
+
+        toolbar = getToolbar();
+        toolbar.setNavigationIcon(R.drawable.splashlogo);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open,
+                R.string.drawer_close){
+            /* Called when drawer is closed */
+            public void onDrawerClosed(View view) {
+                //Put your code here
+            }
+
+            /* Called when a drawer is opened */
+            public void onDrawerOpened(View drawerView) {
+                //Put your code here
+                Utils.hideKeyboard(HomeActivity.this);
+            }
+        };
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(false);
+        drawerToggle.setDrawerIndicatorEnabled(false);
+        drawerToggle.setHomeAsUpIndicator(R.drawable.icon_menu);
+
+        //drawerToggle.setHomeAsUpIndicator(R.drawable.splashlogo);
+        //drawerToggle.setDrawerIndicatorEnabled(false);
+        //drawerLayout.addDrawerListener(drawerToggle);
+       // drawerToggle.syncState();
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       // getSupportActionBar().setHomeButtonEnabled(true);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //drawerToggle.setDrawerIndicatorEnabled(true);
+        //drawerToggle.syncState();
+    }
+}
